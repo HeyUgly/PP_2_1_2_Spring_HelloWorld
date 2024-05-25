cfe6e1615a2131fdbed71f5a10770e65f535e614
HeyUgly <heyuglysnail@gmail.com>
2024-05-25 14:30:52 +0300

    PP_2_1_2_Spring_HelloWorld
    Michael Chursin

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld firstBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(firstBean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(secondBean.getMessage());
+
+        System.out.println(firstBean == secondBean);
+
+        System.out.println();
+
+        Cat firstCatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCatBean.getMessage());
+        Cat secondCatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(secondCatBean.getMessage());
+
+        System.out.println(firstCatBean == secondCatBean);
     }
 }
